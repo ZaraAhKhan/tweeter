f5d3406770d31f0d9f934571f21dd95c95c73071
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-12-15 10:50:08 -0500

    add timeago library and refactor html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,6 +30,7 @@ const initialTweets = [
 ];
 
 const createTweetElement = function(tweet) {
+  let time = timeago.format(tweet.created_at)
   const tweetElement = `
   <article class ="tweet">
     <header>
@@ -44,7 +45,7 @@ const createTweetElement = function(tweet) {
       <br>
     <hr/>
   <footer>
-      <span>${tweet.created_at} days ago</span> 
+      <span>${time}</span> 
       <div class="tweet-icons">
       <span><i class="fa-solid fa-flag"></i></span>
       <span><i class="fa-solid fa-retweet"></i></span>
@@ -56,14 +57,18 @@ const createTweetElement = function(tweet) {
 
 
 const renderTweetElements = function(target) {
+  //iterate over the initialtweets object
   for (let tweet of initialTweets) {
+    // pass to a function to create a tweet element
     const newElement = createTweetElement(tweet);
+    // attach the element to the DOM
     target.append(newElement);
 
   }
 };
 
 $(document).ready(function() {
+  // get the element where the new element needs to be attached
   const tweetContainer = $('#tweet-container');
   renderTweetElements(tweetContainer);
 
